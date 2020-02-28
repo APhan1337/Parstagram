20ea40c5072ec93e4cb913a998ff827a78734a74
Alex V Phan <avp006@ucsd.edu>
2020-02-27 19:41:35 -0800

    Add stay logged in code in AppDelegate.swift

--- Parstagram/LoginViewController.swift
+++ Parstagram/LoginViewController.swift
@@ -15,7 +15,7 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        passwordField.isSecureTextEntry = true
         // Do any additional setup after loading the view.
     }
     
