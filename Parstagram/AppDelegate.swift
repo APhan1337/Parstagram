d7c7111ec804b4dc4a5ebc0349af8b83a98c29e7
APhan1337 <avp006@ucsd.edu>
2020-03-03 15:29:01 -0800

    Add logout code

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -12,8 +12,6 @@ import Parse
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
@@ -23,13 +21,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 configuration.server = "https://safe-brook-54417.herokuapp.com/parse"
             })
         )
-        
-        if PFUser.current() != nil {
-            let main = UIStoryboard(name: "Main", bundle: nil)
-            let feedNavigationController = main.instantiateViewController(identifier: "FeedNavigationController")
-
-            window?.rootViewController = feedNavigationController
-        }
         
         return true
     }
--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -71,4 +71,14 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     */
 
+    @IBAction func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(identifier: "LoginViewController")
+        let sceneDelegate = self.view.window?.windowScene?.delegate as! SceneDelegate
+        
+        sceneDelegate.window?.rootViewController = loginViewController
+        
+    }
 }
